45829162bc1ad7997999a9e0f0238831bc0f192a
Abdul Rojak <bigzero55@outlook.com>
2025-04-29 19:38:01 +0700

    prod2

---
+++ pm2.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  apps: [
+    {
+      name: "landig-page-affiliate",
+      script: "node_modules/next/dist/bin/next",
+      args: "start",
+      instances: 1,
+      exec_mode: "cluster",
+      autorestart: true,
+      watch: false,
+      max_memory_restart: "1G",
+      env: {
+        NODE_ENV: "production",
+        PORT: 3000,
+      },
+    },
+  ],
+};
